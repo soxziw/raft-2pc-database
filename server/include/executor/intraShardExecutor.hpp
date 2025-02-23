27bee56d0ccb7ec89e0e33ca87064cb2239dd63d
soxziw <eric.xu22719@gmail.com>
2025-02-23 18:52:36 +0000

    [Feat] comment executor header

--- server/include/executor/intraShardExecutor.hpp
+++ server/include/executor/intraShardExecutor.hpp
@@ -4,8 +4,18 @@
 #include "raftState.hpp"
 #include "asyncIO.hpp"
 
-
+/**
+ * IntraShardExecutor - Executor of intra shard messages.
+ */
 class IntraShardExecutor {
 public:
+    /**
+     * executeReq - Process request.
+     *
+     * @param client_socket sender of the request
+     * @param aio pointer to async I/O layer
+     * @param raft_state pointer to raft state object
+     * @param req protobuf request
+     */
     static void executeReq(int client_socket, std::shared_ptr<AsyncIO> aio, std::shared_ptr<RaftState> raft_state, const IntraShardReq& req);
 };
