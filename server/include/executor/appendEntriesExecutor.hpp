d2b3337beac6c9247ebb8e366594d9b802cccb58
soxziw <eric.xu22719@gmail.com>
2025-02-23 18:52:36 +0000

    [Feat] comment executor header

--- server/include/executor/appendEntriesExecutor.hpp
+++ server/include/executor/appendEntriesExecutor.hpp
@@ -4,10 +4,28 @@
 #include "raftState.hpp"
 #include "asyncIO.hpp"
 
-
+/**
+ * AppendEntriesExecutor - Executor of append entries messages.
+ */
 class AppendEntriesExecutor {
 public:
+    /**
+     * executeReq - Process request.
+     *
+     * @param client_socket sender of the request
+     * @param aio pointer to async I/O layer
+     * @param raft_state pointer to raft state object
+     * @param req protobuf request
+     */
     static void executeReq(int client_socket, std::shared_ptr<AsyncIO> aio, std::shared_ptr<RaftState> raft_state, const AppendEntriesReq& req);
 
+    /**
+     * executeRsp - Process response.
+     *
+     * @param client_socket sender of the response
+     * @param aio pointer to async I/O layer
+     * @param raft_state pointer to raft state object
+     * @param rsp protobuf response
+     */
     static void executeRsp(int client_socket, std::shared_ptr<AsyncIO> aio, std::shared_ptr<RaftState> raft_state, const AppendEntriesRsp& rsp);
 };
--- server/include/executor/crossShardExecutor.hpp
+++ server/include/executor/crossShardExecutor.hpp
@@ -4,8 +4,18 @@
 #include "raftState.hpp"
 #include "asyncIO.hpp"
 
-
+/**
+ * CrossShardExecutor - Executor of cross shard messages.
+ */
 class CrossShardExecutor {
 public:
+    /**
+     * executeReq - Process request.
+     *
+     * @param client_socket sender of the request
+     * @param aio pointer to async I/O layer
+     * @param raft_state pointer to raft state object
+     * @param req protobuf request
+     */
     static void executeReq(int client_socket, std::shared_ptr<AsyncIO> aio, std::shared_ptr<RaftState> raft_state, const CrossShardReq& req);
 };
