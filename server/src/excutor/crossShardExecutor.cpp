1fc63b4328158ae9d5a47722a6ebe94ab37aef10
soxziw <eric.xu22719@gmail.com>
2025-03-01 08:38:56 +0000

    [Fix] fix bugs

--- server/src/excutor/crossShardExecutor.cpp
+++ server/src/excutor/crossShardExecutor.cpp
@@ -30,10 +30,24 @@ void CrossShardExecutor::executeReq(int client_socket, std::shared_ptr<AsyncIO> 
             if (entry.id == req.id()) {
                 if (entry.command.substr(0, 9) == "[PREPARE]") {
                     std::printf("[%d:%d][CrossShardReq:%d] Transaction id in prepare phase exists.\n", raft_state->cluster_id_, raft_state->server_id_, raft_state->current_term_);
+                    // Generate response
+                    WrapperMessage* wrapper_msg = new WrapperMessage;
+                    CrossShardRsp* rsp = wrapper_msg->mutable_crossshardrsp();
+                    rsp->set_result(CrossShardResultType::NO);
+                    rsp->set_id(req.id());
+                    std::printf("[%d:%d][DETAIL] CrossShardRsp: result=%d, id=%d\n", raft_state->cluster_id_, raft_state->server_id_, rsp->result(), rsp->id());
+                    aio->add_write_request_msg(client_socket, wrapper_msg, AIOMessageType::NO_RESPONSE);
                     return;
                 }
                 if (entry.command.substr(0, 7) == "[ABORT]") {
                     std::printf("[%d:%d][CrossShardReq:%d] Transaction id already aborted.\n", raft_state->cluster_id_, raft_state->server_id_, raft_state->current_term_);
+                    // Generate response
+                    WrapperMessage* wrapper_msg = new WrapperMessage;
+                    CrossShardRsp* rsp = wrapper_msg->mutable_crossshardrsp();
+                    rsp->set_result(CrossShardResultType::NO);
+                    rsp->set_id(req.id());
+                    std::printf("[%d:%d][DETAIL] CrossShardRsp: result=%d, id=%d\n", raft_state->cluster_id_, raft_state->server_id_, rsp->result(), rsp->id());
+                    aio->add_write_request_msg(client_socket, wrapper_msg, AIOMessageType::NO_RESPONSE);
                     return;
                 }
             }
@@ -63,6 +77,13 @@ void CrossShardExecutor::executeReq(int client_socket, std::shared_ptr<AsyncIO> 
         for (const auto& entry : raft_state->log_) {
             if (entry.id == req.id() && entry.command.substr(0, 8) == "[COMMIT]") {
                 std::printf("[%d:%d][CrossShardReq:%d] Transaction id in commit phase exists.\n", raft_state->cluster_id_, raft_state->server_id_, raft_state->current_term_);
+                // Generate response
+                WrapperMessage* wrapper_msg = new WrapperMessage;
+                CrossShardRsp* rsp = wrapper_msg->mutable_crossshardrsp();
+                rsp->set_result(CrossShardResultType::ACK);
+                rsp->set_id(req.id());
+                std::printf("[%d:%d][DETAIL] CrossShardRsp: result=%d, id=%d\n", raft_state->cluster_id_, raft_state->server_id_, rsp->result(), rsp->id());
+                aio->add_write_request_msg(client_socket, wrapper_msg, AIOMessageType::NO_RESPONSE);
                 return;
             }
         }
@@ -99,6 +120,21 @@ void CrossShardExecutor::executeReq(int client_socket, std::shared_ptr<AsyncIO> 
         aio->add_write_request_msg(client_socket, wrapper_msg, AIOMessageType::NO_RESPONSE);
     } else { // Abort phase
         std::printf("[%d:%d][CrossShardReq:%d] Abort transaction.\n", raft_state->cluster_id_, raft_state->server_id_, raft_state->current_term_);
+        // Double check transaction id and phase
+        for (const auto& entry : raft_state->log_) {
+            if (entry.id == req.id() && entry.command.substr(0, 7) == "[ABORT]") {
+                std::printf("[%d:%d][CrossShardReq:%d] Transaction id in commit phase exists.\n", raft_state->cluster_id_, raft_state->server_id_, raft_state->current_term_);
+                // Generate response
+                WrapperMessage* wrapper_msg = new WrapperMessage;
+                CrossShardRsp* rsp = wrapper_msg->mutable_crossshardrsp();
+                rsp->set_result(CrossShardResultType::ACK);
+                rsp->set_id(req.id());
+                std::printf("[%d:%d][DETAIL] CrossShardRsp: result=%d, id=%d\n", raft_state->cluster_id_, raft_state->server_id_, rsp->result(), rsp->id());
+                aio->add_write_request_msg(client_socket, wrapper_msg, AIOMessageType::NO_RESPONSE);
+                return;
+            }
+        }
+
         // Append abort log entry
         raft_state->log_.push_back(
             LogEntry {
--- server/src/excutor/intraShardExecutor.cpp
+++ server/src/excutor/intraShardExecutor.cpp
@@ -25,6 +25,13 @@ void IntraShardExecutor::executeReq(int client_socket, std::shared_ptr<AsyncIO> 
     for (const auto& entry : raft_state->log_) {
         if (entry.id == req.id()) {
             std::printf("[%d:%d][IntraShardReq:%d] Transaction id exists.\n", raft_state->cluster_id_, raft_state->server_id_, raft_state->current_term_);
+            // Generate response
+            WrapperMessage* wrapper_msg = new WrapperMessage;
+            IntraShardRsp* rsp = wrapper_msg->mutable_intrashardrsp();
+            rsp->set_result(IntraShardResultType::FAIL);
+            rsp->set_id(req.id());
+            std::printf("[%d:%d][DETAIL] IntraShardRsp: result=%d, id=%d\n", raft_state->cluster_id_, raft_state->server_id_, rsp->result(), rsp->id());
+            aio->add_write_request_msg(client_socket, wrapper_msg, AIOMessageType::NO_RESPONSE);
             return;
         }
     }
